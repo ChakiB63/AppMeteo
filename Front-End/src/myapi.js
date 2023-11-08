36360ada5d129d77968940082c21a8f4396a0860
ChakiB63 <110175811+ChakiB63@users.noreply.github.com>
2023-11-08 16:57:26 +0100

    Update myapi.js

--- Front-End/src/myapi.js
+++ Front-End/src/myapi.js
@@ -1,6 +1,6 @@
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = "8c10038ece0834bef18a107cde7070bb";
+export const WEATHER_API_KEY = "***WEATHER_API_KEY***";
 export const MyAPI_URL = "http://localhost:5000";
 
 export const geoApiOptions = {
